c3b177ffe59d51216c02ded672a9111fd808f1c5
Brian Ly <brian.kyang.ly@gmail.com>
2019-07-20 19:07:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,4 +55,18 @@ class TicTacToe
       "O"
     end
   end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      token = current_player
+      move(index, token)
+      display_board
+    else
+      turn
+    end
+  end
+  
 end
