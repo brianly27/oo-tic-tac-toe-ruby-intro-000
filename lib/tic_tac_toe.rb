ad2a8b4febcfd559411b602cfd2a483e39f9d5fe
Brian Ly <brian.kyang.ly@gmail.com>
2019-07-20 18:55:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,7 @@ class TicTacToe
   def turn_count
     counter = 0
     @board.each { |position|
-      if position == "X" || position == "O"]
+      if position == "X" || position == "O"
         counter += 1
       end
     }
